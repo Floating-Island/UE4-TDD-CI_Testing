6b672089c76617cf737e49f8b89ec8b2a1c2546e
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-08-26 09:47:25 -0300

    Add project files.

---
+++ Source/CITesting/CITestingGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CITestingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CITESTING_API ACITestingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
