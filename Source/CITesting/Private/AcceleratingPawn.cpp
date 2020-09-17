8a056e5edf43e16a68580cd36fe13a2fd6069f22
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-09-17 16:30:03 -0300

    Added an AcceleratingPawn.

---
+++ Source/CITesting/Private/AcceleratingPawn.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AcceleratingPawn.h"
+
+// Sets default values
+AAcceleratingPawn::AAcceleratingPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAcceleratingPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAcceleratingPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AAcceleratingPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
