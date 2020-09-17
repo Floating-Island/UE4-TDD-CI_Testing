c49b6d7bfc0cb5ad7e7ea19aff1f7b78c494214a
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-09-17 17:14:32 -0300

    Created a mock to use in tests.

---
+++ Source/CITesting/Tests/Mocks/AcceleratingPawnMOCK.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AcceleratingPawn.h"
+#include "AcceleratingPawnMOCK.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CITESTING_API AAcceleratingPawnMOCK : public AAcceleratingPawn
+{
+	GENERATED_BODY()
+	
+};
