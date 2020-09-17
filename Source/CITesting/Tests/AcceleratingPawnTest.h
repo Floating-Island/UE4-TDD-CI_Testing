c8d85fb5479646c5c9cac51f8d7589102bc71546
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-09-17 16:34:27 -0300

    Created the test class for the AcceleratingPawn.

---
+++ Source/CITesting/Tests/AcceleratingPawnTest.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class CITESTING_API AcceleratingPawnTest
+{
+public:
+	AcceleratingPawnTest();
+	~AcceleratingPawnTest();
+};
