6f4cdce31066e11c23b90a92d9f808ab2fb4fa9b
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-09-17 16:36:21 -0300

    Added test structure.

--- Source/CITesting/Tests/AcceleratingPawnTest.cpp
+++ Source/CITesting/Tests/AcceleratingPawnTest.cpp
@@ -3,3 +3,49 @@
 
 #include "AcceleratingPawnTest.h"
 
+#include "Misc/AutomationTest.h"
+
+
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+//each one of this tests should test something of the project class that this test class references to.
+//Each project class should have a test class for it. It's something kind of necessary for TDD.
+
+//It's nice if the prettyname follows a pattern like: Game.Unit.ClassToTest.TestName
+//TestName should express what you expect from a test given a scenario.
+//Pay attention to the automation flags because they're needed to run the tests without UI errors.
+
+
+//a test
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FOneShouldBeOneTest, "Game.Unit.TestPlaceholder.OneShouldBeOne", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+bool FOneShouldBeOneTest::RunTest(const FString& Parameters)
+{
+	//One should be one.
+	{
+		TestEqual(TEXT("one is one"), 1, 1);
+	}
+
+	return true;
+}
+
+
+//another test
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FSTwoShouldBeTwoTest, "Game.Unit.TestPlaceholder.TwoShouldBeTwo", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+bool FSTwoShouldBeTwoTest::RunTest(const FString& Parameters)
+{
+	//two should be two.
+	{
+		TestEqual(TEXT("two is two"), 2, 2);
+	}
+
+	return true;
+}
+
+#endif //WITH_DEV_AUTOMATION_TESTS
