8050913b78160e464698fe0189b411d8f738e391
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-09-18 09:34:47 -0300

    Added UnrealEd module to be able to use editor modules.

--- Source/CITesting/CITesting.Build.cs
+++ Source/CITesting/CITesting.Build.cs
@@ -8,7 +8,7 @@ public class CITesting : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UnrealEd" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
